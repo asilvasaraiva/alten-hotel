53bfeb6ed677776e324858bc0519d13bba8f2b10
Alexsandro Saraiva <alex_saraiva14@hotmail.com>
2022-12-15 18:51:15 -0300

    Inicial Upload

---
+++ src/main/java/com/api/alten/hotel/HotelCancunApplication.java
@@ -0,0 +1,13 @@
+package com.api.alten.hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelCancunApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelCancunApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/alten/hotel/HotelCancunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.alten.hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelCancunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
